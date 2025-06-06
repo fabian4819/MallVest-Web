79fc8dd5bf3b8a5861a23ada08460268dfc59865
fabian4819 <habibfabianfahlesi2004@Mail.ugm.ac.id>
2025-06-06 23:21:34 +0700

    fix: change contract address

--- src/contracts/contracts.ts
+++ src/contracts/contracts.ts
@@ -12,19 +12,19 @@ import { ContractType, ABISingle, AvailableContracts } from "./types";
 const contracts: ContractType[] = [
   {
     name: "MockUSDC",
-    address: "0xC425c96B30BF8a9190E7A273D990a6a8B6F49C3b",
-    abi: MockUSDC.abi as ABISingle[]
+    address: "0x788A80c50cc0a16fDCE6f011EeB0df60F96291B5",
+    abi: MockUSDC.abi as ABISingle[],
   },
   {
     name: "LaLoHotelRegistry",
-    address: "0xc84D1e8FECaDa44487242E5D855AEE7F752A12EA",
-    abi: LaLoHotelRegistry.abi as ABISingle[]
+    address: "0x3F9452B299fE5D75B55f7A180DEEE777A78966Cf",
+    abi: LaLoHotelRegistry.abi as ABISingle[],
   },
   {
     name: "LaLoHotelTokenization",
-    address: "0x2dFf6D5eB709b368df0c11bd80209eB92591658c",
-    abi: LaLoHotelTokenization.abi as ABISingle[]
-  }
+    address: "0xF9d0d3B21e3BDA134b87CEe076EDa0Af2664E26A",
+    abi: LaLoHotelTokenization.abi as ABISingle[],
+  },
 ];
 
 export const getContract = (name: AvailableContracts) => {
